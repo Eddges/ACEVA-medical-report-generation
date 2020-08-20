de7adf376c3921f263122a2f91b8653e54fa9966
Eddges <shivamshekhar.i7@gmail.com>
2020-08-20 14:58:52 +0530

    skeleton

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -1,15 +1,78 @@
 import React from 'react'
 import classes from './Report.module.css'
 import ReportField from '../ReportField/ReportField'
+import Logo from '../../assets/ChiroHDLogo.png'
+
 
 class Report extends React.Component{
     render(){
         return(
             <div className={classes.Container}>
-            Report
-                <ReportField />
-                <ReportField />
-                <ReportField />
+                <div className={classes.Report}>
+                    <div className={classes.Top}>
+
+                        <img src={Logo} alt="Logo" />
+
+                        <div className={classes.Search}>
+                            <input placeholder="Search Record" />
+                        </div>
+
+                        <div className={classes.Select}>
+                            <select>
+                                <option>Report</option>
+                                <option>Supplements</option>
+                            </select>
+                        </div>
+
+                        <div className={classes.LabelFilter}>
+                            {/* <div className={classes.Filter}></div> */}
+                            <i className="fas fa-eye" style={{'color' : '#0075f6'}}></i>
+                            <div className={classes.VerticalLine}></div>
+                        </div>
+
+                    </div>
+
+                    <div className={classes.Mid}>
+                        <div className={classes.MidTop}>
+
+                            <i className={`${classes.ItemIcon} ${"fas fa-file-medical"}`} style={{'color' : '#0075f6'}}></i>
+                            <span className={classes.Item}>Field</span>
+                            <span className={classes.Item}>Current</span>
+                            <span className={classes.Item}>Previous</span>
+                            <span className={classes.Item}>Status</span>
+                            <span className={classes.Item}>Reference</span>
+
+                        </div>
+
+                        <div className={classes.MidReport}>
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+                            <ReportField />
+
+                        </div>
+                        
+                    </div>
+                    {/* <ReportField />
+                    <ReportField />
+                    <ReportField /> */}
+                </div>
+                
 
             </div>
         )
--- src/components/ReportField/ReportField.js
+++ src/components/ReportField/ReportField.js
@@ -4,7 +4,12 @@ import classes from './ReportField.module.css'
 const ReportField = (props) => {
     return (
         <div className={classes.Container}>
-            <span className={classes.Title}>Vitamin B12</span>
+            <i className={`${classes.Icon} ${"fas fa-burn"}`} style={{'color' : '#0075f6'}}></i>
+            <span className={`${classes.Item}`}>Haemoglobin</span>
+            <span className={`${classes.Item} ${classes.CurrDate}`}> <span>8%</span>12/08/2020</span>
+            <span className={`${classes.Item} ${classes.PrevDate}`}><span>15%</span>14/07/2020 </span>
+            <span className={`${classes.Item} ${classes.Status}`}>Critical</span>
+            <span className={`${classes.Item} ${classes.Reference}`}>40%-50%</span>
         </div>
     )
 }
