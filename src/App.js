a3319687269bb4c4fc229d836c00f7546c9ef43b
nagendra0032 <nagendrakumar0032@gmail.com>
2020-08-24 13:59:43 +0530

    form page integrated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import MedReport from './components/MedReport/MedReport'
 import './app.css'
+import FormPage from './components/Form_page/form.component';
 
 class App extends React.Component{
     render(){
 
         return(
             <div>
-                <MedReport />            
+                <FormPage/>
+                {/* <MedReport />             */}
             </div>
         )
     }
