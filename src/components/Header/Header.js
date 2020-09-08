736a2664ce4ea89d7a415f22bb2fcb0035da775f
nagendra0032 <nagendrakumar0032@gmail.com>
2020-09-08 16:12:06 +0530

    header pushed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './Header.css';
+import flogo from '../../assets/facebook.png';
+import tlogo from '../../assets/twitter.png';
+import ilogo from '../../assets/insta.png';
+import plogo from '../../assets/phone.png';
+import ylogo from '../../assets/youtube.png';
+import ulogo from '../../assets/user.png';
 
 function Header() {
     return (
         <div className = "header_wrapper">
-            
+            <div className = "header_content">
+                <div className = "header_left">
+                    <img src = {flogo}/>
+                    <img src = {tlogo}/>
+                    <img src = {ylogo}/>
+                    <img src = {ilogo}/>
+                    
+                    <p>(877) 688-7241</p>
+
+                </div>
+                <div className = "header_right">
+                    <img src = {ulogo}/>
+                    <p>Login</p>
+                    <p className = "reg">REGISTER to Start Learning Point</p>
+                </div>
+            </div>
         </div>
     )
 }
